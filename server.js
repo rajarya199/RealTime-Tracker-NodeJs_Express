e3b1d0dc8b4f057f3cfa3ee471d63b014df0f8b5
rajarya199 <aryalrajan078@gmail.com>
2025-03-31 20:04:28 +0545

    setup socket io,

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express=require("express");
 const app=express();
+const http=require("http") //socket io use htttp
+const socketio=require("socket.io")
+const server=http.createServer(app) //create server using http
+const io=socketio(server) //create socket using server
+
+
 app.get("/",function(req,res){
 res.send("hey buddy")
 })
 
-app.listen(3000)
+server.listen(3000)
