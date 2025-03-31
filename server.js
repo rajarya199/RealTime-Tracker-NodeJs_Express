1b19053c1a9c9a88a1189fb49bc86188bf1b6829
rajarya199 <aryalrajan078@gmail.com>
2025-04-01 00:07:12 +0545

    send-receive action

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ const server=http.createServer(app) //create server using http
 const io=socketio(server) //create socket using server
 
 app.set("view engine","ejs") //set view engine to ejs
-app.set(express.static(path.join(__dirname,"public"))) //set static folder to public
+app.use(express.static(path.join(__dirname,"public"))) //set static folder to public
 
 //handle socket connection
 io.on("connection",(socket)=>{
-console.log("a user connected") 
+    //receive location form client and sent location with socket id to client.frontend
+    socket.on("send-location",function(data){
+        io.emit("receive-location",{id:socket.id,...data})
+    })
+console.log(" user connected") 
 })
 
 app.get("/",function(req,res){
