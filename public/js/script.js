2304010308e2e2aedc26c58978c18d6580e86d1e
rajarya199 <aryalrajan078@gmail.com>
2025-03-31 23:46:02 +0545

    track loction,send to server

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,28 @@
- //initialize socket.io
- const socket=io(); //connect to socket.io server
+//initialize socket.io
+const socket = io(); //connect to socket.io server
+
+//if browser support geoloaction,watch position to track
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      //emit location to server
+      socket.emit("send location", { latitude, longitude });
+    },
+    (error) => {
+      console.error("Error getting location: ", error);
+    },
+    {
+      enableHighAccuracy: true,
+      maximumAge: 0, //no caching
+      timeout: 5000,
+    }
+  );
+}
+
+
+ const map=L.map("mapbox").setView([0,0],10)
+L.tilelayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+attribution:"openStreet map"
+}).addTo(map)
